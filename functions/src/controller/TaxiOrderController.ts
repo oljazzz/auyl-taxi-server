4d797bfc533ee60375ac479967523d01de4a24ce
oljazzz <oljazzz362@gmail.com>
2021-05-12 16:43:43 +0600

    patch method fixed

--- functions/src/controller/TaxiOrderController.ts
+++ functions/src/controller/TaxiOrderController.ts
@@ -1,11 +1,13 @@
-import { Response } from 'express'
+import {Response} from 'express'
 import Request from '../types/RequestType'
-import { db } from '../config/firebase'
+import {db} from '../config/firebase'
 import TaxiOrderType from '../types/TaxiOrderType'
 
 const addTaxiOrder = async (req: Request, res: Response) => {
-    const { directionFrom, directionTo,
-        amount, clientPhoneNumber } = req.body
+    const {
+        directionFrom, directionTo,
+        amount, clientPhoneNumber
+    } = req.body
     try {
         const taxiOrder = db.collection('orders').doc()
         const taxiOrderObject = {
@@ -52,17 +54,18 @@ const getOrder = async (req: Request, res: Response) => {
     }
 }
 
-const updateOrder = async(req: Request, res: Response)=>{
+const updateOrder = async (req: Request, res: Response) => {
     const {body: {directionFrom, directionTo, amount}, params: {orderId}} = req
     try {
         const order = db.collection('orders').doc(orderId)
         const currentData = (await order.get()).data() || {}
         const orderObject = {
+            id: order.id,
             directionFrom: directionFrom || currentData.directionFrom,
             directionTo: directionTo || currentData.directionTo,
             amount: amount || currentData.amount,
         }
-        await order.set(orderObject).catch(error=>{
+        await order.set(orderObject).catch(error => {
             return res.status(400).json({status: 'error', message: error.message})
         })
         return res.status(200).json({
@@ -70,9 +73,9 @@ const updateOrder = async(req: Request, res: Response)=>{
             message: 'order updated successfully',
             data: orderObject
         })
-    } catch(error){
+    } catch (error) {
         return res.status(500).json(error.message)
     }
 }
 
-export { addTaxiOrder, getAllOrders, getOrder, updateOrder}
+export {addTaxiOrder, getAllOrders, getOrder, updateOrder}
